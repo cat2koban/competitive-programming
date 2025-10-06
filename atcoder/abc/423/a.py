4d8209dbf48f66a6a09cf3c2950ccf2af5c9aabf
cat2koban <cat2koban@users.noreply.github.com>
2025-10-06 23:19:07 +0900

    Solve abc/423/a

--- atcoder/abc/423/a.py
+++ atcoder/abc/423/a.py
@@ -1,16 +1,2 @@
 x,c = map(int, input().split())
-for i in range(x, -1000, -1000):
-    ii = i // 1000
-    j = x - ii * 1000
-    print(j)
-    pre = ii * (c / 1000)
-    print(ii)
-    print(pre)
-    if pre+ii <= x:
-        if ii>=c:
-            print(ii)
-            break
-        else:
-            print(0)
-            break
-
+print((x // (1000 + c))*1000)
